8ed45c643b86d3561b69cda1872224070c8f1f3f
Pradhan <chrisonpradhan@gmail.com>
2022-08-05 15:46:51 +0530

    add nfts

--- src/components/AllNfts.jsx
+++ src/components/AllNfts.jsx
@@ -7,7 +7,7 @@ const AllNfts = () => {
 
  const fetchNft = async () => {
   const url = `
-https://deep-index.moralis.io/api/v2/nft/${nftTokenAddress}?chain=rinkeby&format=decimal`;
+  https://deep-index.moralis.io/api/v2/nft/${nftTokenAddress}/owners?chain=rinkeby&format=decimal&limit=4`;
 
   axios(url, {
    method: "GET",
