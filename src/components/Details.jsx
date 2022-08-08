ed7dae414383cd7ab68e7eb02c5dde6e1836ea4c
shubhamkr95 <sahooshubham124@gmail.com>
2022-08-08 11:50:05 +0530

    chnages in length of prop id

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -49,11 +49,11 @@ const Details = (prop) => {
       <p className=" font-extrabold text-gray-50 ml-2">{data.proposer_address}</p>
      </div>
      <div>
-      <p className=" font-medium text-xl text-gray-400 mt-5">Proposer Address: {data.proposer_address}</p>
+      <p className="font-xs md:font-medium text-xl text-gray-400 mt-5">Proposer Address: {data.proposer_address}</p>
       <p className=" font-medium text-xl text-gray-200 mt-5">Quorum Required: {QuorumPercentage} Percentage</p>
       <p className=" font-medium text-xl text-gray-400 mt-5">Proposal Threshold: {ProposalThreshold} GTK</p>
       <p className=" font-medium text-xl text-gray-200 mt-5">
-       Proposal ID: <span className="font-small text-xl">{`${data.proposal_id.slice(0, 50)}`}</span>
+       Proposal ID: <span className="font-xs md:font-small  text-xl">{`${data.proposal_id.slice(0, 50)}`}</span>
       </p>
      </div>
      <div className=" mt-5  text-3xl font-bold text-gray-50">
